6f617e1f07134192f57d4da4d6a713dfeccc7876
FSanchez89 <franciscoelz@gmail.com>
2023-04-26 16:33:23 +0200

    Funcion login creada con exito y ahorita trabajando con pagina perfil

--- datosperfil.js
+++ datosperfil.js
@@ -1,54 +1,47 @@
-var usuario =
-{
-    id: 003,
-    nombreDeUsuario: "Messi151",
-    contraseña: "contraseña3",
-    correoElectronico: "Messi@example.com",
-    nombreCompleto: "Lionel Messi",
-    pais: "Francia",
-    ciudad: "Paris",
-    edad: 34,
-    estudios: "Graduado en la Universidad de la vida, Certificado en Marketing Digital por la escaloneta",
-    idiomas: ["Español", "Inglés", "Francés"],
-    perfilLinkedIn: "https://www.linkedin.com/in/Messi/",
-    hobbies: ["Fútbol, Ganar copas del mundo", "Tomar mate"]
-}
+// Obtener los datos del perfil del usuario
+window.onload = function () {
+  // Obtener los datos del perfil del usuario
+  fetch('/datosperfil')
+    .then(function (response) {
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(function (datosPerfil) {
+      // Actualizar los elementos del HTML con los datos del perfil
+      console.log(response);
+      document.getElementById('nombre-completo').textContent = datosPerfil.fullname;
+      document.getElementById('ciudad').textContent = datosPerfil.city;
+      document.getElementById('pais').textContent = datosPerfil.country;
+      document.getElementById('edad').textContent = datosPerfil.age;
+      document.getElementById('estudios').textContent = datosPerfil.university;
+      document.getElementById('idiomas').textContent = datosPerfil.languages;
+      document.getElementById('linkedin').textContent = datosPerfil.linkedin;
+      document.getElementById('hobbies').textContent = datosPerfil.hobbies;
+    })
+    .catch(function (error) {
+      console.log('Error al recuperar los datos del perfil:');
+    });
+};
 
-var nombreCompleto = document.getElementById("nombre-completo");
-var pais = document.getElementById("pais");
-var ciudad = document.getElementById("ciudad");
-var edad = document.getElementById("edad");
-var estudios = document.getElementById("estudios");
-var idiomas = document.getElementById("idiomas");
-var linkedin = document.getElementById("linkedin");
-var hobbies = document.getElementById("hobbies");
 
-// Asigna los valores correspondientes a cada elemento HTML
-nombreCompleto.textContent = usuario.nombreCompleto;
-pais.textContent = usuario.pais;
-ciudad.textContent = usuario.ciudad;
-edad.textContent = usuario.edad;
-estudios.textContent = usuario.estudios;
-idiomas.textContent = usuario.idiomas.join(", ");
-linkedin.textContent = usuario.perfilLinkedIn
-linkedin.setAttribute("href", usuario.perfilLinkedIn);
-hobbies.textContent = usuario.hobbies.join(", ");
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////
 // Agregar evento de clic al botón de eliminar cuenta
 document.getElementById("eliminar-cuenta").addEventListener("click", function () {
-    var confirmacion = confirm("¿Estás seguro de que quieres eliminar tu cuenta? Esta acción no se puede deshacer.");
+  var confirmacion = confirm("¿Estás seguro de que quieres eliminar tu cuenta? Esta acción no se puede deshacer.");
 
-    if (confirmacion) {
-        // Eliminar cuenta y datos asociados
-        usuario = null;
-        document.getElementById("nombre-completo").innerHTML = "";
-        document.getElementById("pais").innerHTML = "";
-        document.getElementById("ciudad").innerHTML = "";
-        document.getElementById("edad").innerHTML = "";
-        document.getElementById("estudios").innerHTML = "";
-        document.getElementById("idiomas").innerHTML = "";
-        document.getElementById("linkedin").innerHTML = "";
-        document.getElementById("hobbies").innerHTML = "";
-        document.getElementById("mensaje-confirmacion").classList.remove("oculto");
-    }
+  if (confirmacion) {
+    // Eliminar cuenta y datos asociados
+    usuario = null;
+    document.getElementById("nombre-completo").innerHTML = "";
+    document.getElementById("pais").innerHTML = "";
+    document.getElementById("ciudad").innerHTML = "";
+    document.getElementById("edad").innerHTML = "";
+    document.getElementById("estudios").innerHTML = "";
+    document.getElementById("idiomas").innerHTML = "";
+    document.getElementById("linkedin").innerHTML = "";
+    document.getElementById("hobbies").innerHTML = "";
+    document.getElementById("mensaje-confirmacion").classList.remove("oculto");
+  }
 });
