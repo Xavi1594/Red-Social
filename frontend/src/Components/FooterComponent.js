de5b6eefffad6fb85eab3f08f6d8388b59649bbe
Xavi1594 <xavi9415@hotmail.com>
2023-05-16 12:50:44 +0200

    instalando react

---
+++ frontend/src/Components/FooterComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const FooterComponent = () => {
+  return (
+    <div>
+        <footer class="container-fluid bg-dark  text-center text-light ">
+        <span class="h5">&copy; Socialy 2023</span>
+    </footer>
+    </div>
+  )
+}
